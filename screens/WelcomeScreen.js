8a0f200f35e854812961f0a709712356ee676cb1
Kartheekbandi <123476304+Kartheekbandi@users.noreply.github.com>
2023-10-13 01:40:22 -0400

    second

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,160 +1,137 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import axios from 'axios';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { AuthContext } from '../store/auth-context';
-import { TouchableOpacity } from 'react-native';
-
+import AddVideo from './AddVideo';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function LocalActivity() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Any events will be posted here!</Text>
+        </View>
+    );
+}
+
+function Tourist({ navigation }) {
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start', padding: 10 }}>
+                <Ionicons name="add-circle-outline" size={42} color="blue"
+                    onPress={() => navigation.navigate('AddVideo')} />
+            </View>
+            <View style={{ position: 'absolute', bottom: 10, right: 10, flexDirection: 'column' }}>
+                <Ionicons name="heart-outline" size={42} color="black" style={styles.icon} />
+                <Ionicons name="chatbubble-outline" size={40} color="black" style={styles.icon} />
+                <Ionicons name="arrow-redo-outline" size={42} color="black" style={styles.icon} />
+                <Ionicons name="bookmark-outline" size={42} color="black" style={styles.icon} />
+            </View>
+        </View>
+    );
+}
+
+function Settings({ navigation }) {
+    const navigateToScreen = (screenName) => {
+        navigation.navigate(screenName);
+    };
+
+    return (
+        <View style={styles.Settings}>
+            <TouchableOpacity onPress={() => navigateToScreen('Posts')}>
+                <View style={styles.item}>
+                    <Text style={styles.Set}>Posts</Text>
+                    <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow}/>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigateToScreen('SavedPosts')}>
+                <View style={styles.item}>
+                    <Text style={styles.Set}>Saved</Text>
+                    <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow} />
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigateToScreen('Likes')}>
+                <View style={styles.item}>
+                    <Text style={styles.Set}>Likes</Text>
+                    <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow} />
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigateToScreen('Comments')}>
+                <View style={styles.item}>
+                    <Text style={styles.Set}>Comments</Text>
+                    <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow} />
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+function TabNavigator() {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Settings') {
+                        iconName = focused ? 'ios-list' : 'ios-list-outline';
+                    } else if (route.name === 'Tourist') {
+                        iconName = focused ? 'film' : 'film-outline';
+                    } else if (route.name === 'Local Activity') {
+                        iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: 'tomato',
+                tabBarInactiveTintColor: 'gray',
+            })}>
+            <Tab.Screen name="Tourist" component={Tourist} />
+            <Tab.Screen name="Local Activity" component={LocalActivity} />
+            <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+    );
+}
 
 function WelcomeScreen() {
-  const [fetchedMessage, setFetchedMesssage] = useState('');
-
-  const authCtx = useContext(AuthContext);
-  const token = authCtx.token;
-
-  useEffect(() => {
-    axios
-      .get(
-        'https://react-native-course-3cceb-default-rtdb.firebaseio.com/message.json?auth=' +
-          token
-      )
-      .then((response) => {
-        setFetchedMesssage(response.data);
-        
-
-      });
-  }, [token]);
-
-  function LocalActivity() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Any events will be posted here!</Text>
-        
-      </View>
+        <Stack.Navigator initialRouteName="MainTabs">
+            <Stack.Screen name="MainTabs" component={TabNavigator} options={{ headerShown: false }} />
+            <Stack.Screen name="AddVideo" component={AddVideo} />
+        </Stack.Navigator>
     );
-  }
-
-  function Tourist() {
-    return (
-      <View style={{ flex: 1}}>
-        <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start', padding: 10  }}>
-          <Ionicons name="add-circle-outline" size={42} color="blue" />
-        </View>
-        <View style={{ position: 'absolute', bottom: 10, right: 10, flexDirection: 'column'}}>
-          <Ionicons name="heart-outline" size={42} color="black" style={styles.icon} />
-          <Ionicons name="chatbubble-outline" size={40} color="black" style={styles.icon} />
-          <Ionicons name="arrow-redo-outline" size={42} color="black" style={styles.icon} />
-          <Ionicons name="bookmark-outline" size={42} color="black" style={styles.icon} />
-          {/* <Ionicons name="warning-outline" size={42} color="black" style={styles.icon} /> */}
-        </View>
-      </View>
-    );
-  }
-  
-
-
-
-  function Settings({ navigation }) {
-    const navigateToScreen = (screenName) => {
-      navigation.navigate(screenName);
-    };
-    
-  
-    return (
-      
-      <View style={styles.Settings}>
-        <TouchableOpacity onPress={() => navigateToScreen('Posts')}>
-          <View style={styles.item}>
-            <Text style={styles.Set}>Posts</Text>
-            <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow}/>
-          </View>
-        </TouchableOpacity>
-  
-        <TouchableOpacity onPress={() => navigateToScreen('SavedPosts')}>
-          <View style={styles.item}>
-            <Text style={styles.Set}>Saved</Text>
-            <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow} />
-          </View>
-        </TouchableOpacity>
-  
-        <TouchableOpacity onPress={() => navigateToScreen('Likes')}>
-          <View style={styles.item}>
-            <Text style={styles.Set}>Likes</Text>
-            <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow} />
-          </View>
-        </TouchableOpacity>
-  
-        <TouchableOpacity onPress={() => navigateToScreen('Comments')}>
-          <View style={styles.item}>
-            <Text style={styles.Set}>Comments</Text>
-            <Ionicons name="arrow-forward" size={22} color="black" style={styles.arrow} />
-          </View>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-  
-
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Settings') {
-            iconName = focused ? 'ios-list' : 'ios-list-outline';
-          } else if (route.name === 'Tourist') {
-            iconName = focused ? 'film' : 'film-outline';
-          } else if (route.name === 'Local Activity') {
-            iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}
-    >
-      <Tab.Screen name="Tourist" component={Tourist} />
-      <Tab.Screen name="Local Activity" component={LocalActivity} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
-  );
 }
 
 const styles = StyleSheet.create({
-  Settings: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    padding: 10,
-  },
-  icon : {
-    padding: 15
-  },
-  item: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    Settings: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        padding: 10,
     },
-  Set: {
-    padding: 10,
-    fontSize: 16, 
-    
-  },
-  arrow: {
-    position:'absolute',
-    top: 10,
-    left: 350,
-  }
-
-
+    icon: {
+        padding: 15
+    },
+    item: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    Set: {
+        padding: 10,
+        fontSize: 16,
+    },
+    arrow: {
+        position: 'absolute',
+        top: 10,
+        left: 350,
+    }
 });
 
 export default WelcomeScreen;
-
-
