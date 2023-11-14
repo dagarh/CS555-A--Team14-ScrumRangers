79aba7675d60c0de8d5070033e0eb10d2807e533
CHANDANA CHEBROLU <60417448+chandanachebrolu@users.noreply.github.com>
2023-11-14 12:09:05 -0500

    Create Activity.test.js

---
+++ mobile-app/screens/Activity.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import Activity from './Activity';
+
+describe('Test Activity Page', () => {
+  test('renders events', () => {
+    const { getByPlaceholderText, getByText  } = render(<Activity />);
+    const textinput = getByPlaceholderText('Enter Location');
+    const pressablebutton = getByText('Search Events');
+    
+    expect(textinput).toBeTruthy();
+    expect(pressablebutton).toBeTruthy();
+  });
+
+});
